9f97b8c019e8bc8820be892ad59b1ee066772d4a
Conrad Stansbury <chstansbury@gmail.com>
2014-10-10 23:35:42 -0700

    Updated the algorithm to trim the total dictionary size used from the absurd 835 terms to a more reasonable 150 of them. Also made changes to normalize matrices appropriately so that the cooccurrence matrices have the same element sum, which is a desirable property.

--- eigenface_anomaly_detection/eigenface.py
+++ eigenface_anomaly_detection/eigenface.py
@@ -1,5 +1,6 @@
 import numpy as np
-from collections import OrderedDict
+import random
+from collections import OrderedDict, Counter
 from itertools import chain
 
 def userNames(n):
@@ -12,6 +13,18 @@ def getFileFromName(name):
 def userFilesMap(n):
     return { name: getFileFromName(name) for name in userNames(n) }
 
+def buildSmallCommandDictionary(command_lists, n_most_common, n_random):
+    """ Build a command dictionary (command -> index) with the n_most_common
+        most common found commands along with an additional n_random randomly
+        selected ones."""
+    c = Counter(chain(*command_lists))
+    filtered_commands = [k for k, v in c.most_common(n_most_common)]
+    rest = list(set(c.keys()).difference(set(filtered_commands)))
+    random.shuffle(rest)
+    selected_commands = filtered_commands + rest[:n_random]
+    return {k: i for k, i in zip(selected_commands, range(len(selected_commands)))}
+
+
 def buildCommandDictionary(command_lists):
     ks = OrderedDict.fromkeys(chain(*command_lists)).keys()
     return {k: i for k, i in zip(ks, range(len(ks)))}
@@ -20,20 +33,34 @@ def splitSequence(s, k):
     for i in xrange(0, len(s), k):
         yield s[i:i+k]
 
+def totalExpectedPairs(k, d):
+    """ Computes the number of expected co-occurrence pairs in a sequence of
+        length k with interaction distance d. Used to compute a normalization
+        factor in buildCooccurrence"""
+    return d * (k - d) + d * (d-1) / 2
+
 def buildCooccurrence(seq, d, dictionary):
     """ Seq is the input sequence of words to parse into the co occurence
         matrix. The maximum distance of interaction is d, and the dictionary
-        is used to translate words to indices."""
+        is used to translate words to indices. This function does not assume
+        that all the encountered words are in the dictionary."""
 
-    k = len(dictionary) # much bigger than I would like. :(
+    k = len(dictionary)
     m = np.zeros((k, k))
     for idx_max in range(len(seq)):
         for idx in range(max(0, idx_max - d), idx_max):
+            if seq[idx] not in dictionary or seq[idx_max] not in dictionary:
+                continue
             x = dictionary[seq[idx]]
             y = dictionary[seq[idx_max]]
             m[x, y] += 1
-    #print "done"
-    return m
+
+    # Compute the appropriate normalization
+    t = np.sum(m)
+    t0 = totalExpectedPairs(len(seq), d)
+    if t/t0 < 0.1:
+        print "OH GOD"
+    return m * (t0/t)
 
 def meanMatrices1(matrices):
     return np.mean(matrices, axis=0)
@@ -52,18 +79,22 @@ if __name__ == "__main__":
     train_length = 5000
     test_length = 10000
     sequence_length = 100
+    keep_most_common = 140 # this seems to be minimal to
+                           #prevent losing "too much" info
+    keep_random = 0
     num_train_seq = train_length/sequence_length
     window_len = 6
 
     users_and_files = userFilesMap(num_users)
 
-    command_dict = buildCommandDictionary(users_and_files.values())
+    command_dict = buildSmallCommandDictionary(users_and_files.values(),
+                                               keep_most_common, keep_random)
 
     # still need to split the data into training sets and test sets
     users_and_files = {u: list(splitSequence(users_and_files[u], sequence_length)) for \
                        u in users_and_files }
 
-    # syphon off the training data and concatenate in one list
+    # siphon off the training data and concatenate in one list
     train_seq = []
     for user in users_and_files:
         train_seq.extend(users_and_files[user][:num_train_seq])
