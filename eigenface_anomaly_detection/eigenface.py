6dc6b3647cc845f25ad228c53632c2054050148c
Conrad Stansbury <chstansbury@gmail.com>
2014-10-15 23:55:29 -0700

    I think this is computing the networks... unless I completely didn't understand their definition of network! D:

--- eigenface_anomaly_detection/eigenface.py
+++ eigenface_anomaly_detection/eigenface.py
@@ -1,5 +1,10 @@
+import scipy.linalg as scl
 import numpy as np
 import random
+import math
+from itertools import permutations
+
+from scipy.sparse.linalg.eigen.arpack import eigsh
 from collections import OrderedDict, Counter
 from itertools import chain
 
@@ -18,8 +23,7 @@ def buildSmallCommandDictionary(command_lists, n_most_common, n_random):
         most common found commands along with an additional n_random randomly
         selected ones."""
     c = Counter(chain(*command_lists))
-    print "cd"
-    print c["cd"]
+
     filtered_commands = [k for k, v in c.most_common(n_most_common)]
     rest = list(set(c.keys()).difference(set(filtered_commands)))
     random.shuffle(rest)
@@ -62,18 +66,65 @@ def buildCooccurrence(seq, d, dictionary):
     t0 = totalExpectedPairs(len(seq), d)
     if t/t0 < 0.1:
         print "OH GOD"
-    return m * (t0/t)
+    tf = t0/t
+    if t == 0:
+        tf = 0
+    return m * tf
 
 def calculateCovariance(vecs):
     vec_mat = np.matrix(vecs)
     return vec_mat.T * vec_mat
 
+def minRep(t):
+    i = t.index(min(t))
+    return t[i:] + t[:i]
+
+def pathInGraph(g,p):
+    if len(p) == 1:
+        return True
+    else:
+        return p[1] in g[p[0]] and pathInGraph(g,p[1:])
+
+def cycleInGraph(g,t):
+    return pathInGraph(g,t + (t[0],))
+
+def buildTuplesFromGraph(g, r):
+    """ Finds all the r-cycles in the directed graph g by brute force."""
+    s = set()
+    for t in permutations(g.keys(), r):
+        if cycleInGraph(g, t):
+            s.add(minRep(t))
+    return s
+
+def buildNetworkLayer(mat, cut, r):
+    k = int(math.sqrt(mat.shape[0]))
+    ind = np.argpartition(mat.ravel(), -r)[-r:]
+    g = {}
+    for i in range(r):
+        idx = ind[i]
+        idx_y = idx % k
+        idx_x = (idx - idx_y) / k
+        if idx_x in g:
+            g[idx_x].append(idx_y)
+        else:
+            g[idx_x] = [idx_y]
+
+    return buildTuplesFromGraph(g, 3)
+
+def calculateNetworks(cooccurs, m_cooccurs, eigen_mats):
+    norm_cooccurs = cooccurs - m_cooccurs
+    n_layers = eigen_mats.shape[0]
+    fis = eigen_mats.dot(norm_cooccurs.ravel())
+    for i in range(n_layers - 1, -1, -1):
+        print buildNetworkLayer(fis[i]*eigen_mats[i], 0, 50)
+
 if __name__ == "__main__":
+    num_eigen = 20
     num_users = 50
     train_length = 5000
     test_length = 10000
     sequence_length = 100
-    keep_most_common = 140 #this seems to be minimal to
+    keep_most_common = 130  #this seems to be minimal to
                            #prevent losing "too much" info
     keep_random = 0
     num_train_seq = train_length/sequence_length
@@ -100,6 +151,9 @@ if __name__ == "__main__":
     norm_vec_cooccurs = [n.ravel() for n in norm_train_cooccurs]
 
     P = calculateCovariance(norm_vec_cooccurs)
+    Pk = P.shape[0]
+
     print "Calculating eigenvectors"
-    eig_vals, eig_vecs = np.linalg.eig(P)
-    print eig_vals
+    #eig_vals, eig_vecs = scl.eigh(P, eigvals = (Pk - num_eigen, Pk - 1))
+    eig_vals, eig_vecs = eigsh(P, num_eigen, which="LM")
+    calculateNetworks(train_cooccurs[0], mean, np.transpose(eig_vecs))
